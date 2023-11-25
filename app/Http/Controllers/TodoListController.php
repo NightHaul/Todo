9d0c92e3eb5e619fe087f9b61f90ad30c3b98d2e
yuya671023 <126576075+yuya671023@users.noreply.github.com>
2023-11-25 16:57:31 +0900

    test

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -10,7 +10,7 @@ class TodoListController extends Controller
     public function index(Request $request)
   {
     $todo_lists = TodoList::all();
- 
+
     return view('todo_list.index', compact('todo_lists'));
   }
 }
